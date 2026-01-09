913c05b891ca425ba995ed84bcb1e490ab9cae3e
Filip <kasprzyckifilip11@gmail.com>
2026-01-09 19:42:36 +0100

    Podział na dwa osobne projekty jeden z klasami i logiką drugi z gui

---
+++ WPF/Dzienniczek/Osoba.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dzienniczek
+{
+    public abstract class Osoba
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Pesel 
+        { 
+            get;
+
+            set
+            {
+                if (value.Length == 11)
+                    Pesel = value;
+            } 
+        }
+
+    }
+}
